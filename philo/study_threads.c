41e2ea8678dafcab73bc079343314791d7d1a68e
Jonas Alberto Pereira <jopereir@c1r2p4.42sp.org.br>
2025-01-10 15:15:21 -0300

    study

--- philo/study_threads.c
+++ philo/study_threads.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   study_threads_mutex.c                              :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jopereir <jopereir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 11:24:55 by jopereir          #+#    #+#             */
-/*   Updated: 2025/01/10 13:02:06 by jopereir         ###   ########.fr       */
+/*   Updated: 2025/01/10 15:10:45 by jopereir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,22 @@
 
 typedef struct s_struct
 {
-	pthread_mutex_t	mutex;
-	int				x;
+	pthread_mutex_t		mutex;
+	unsigned long long	x;
 }	t_struct;
 
 void	*rotine(void *a)
 {
-	t_struct	*b;
-	int			i;
+	t_struct			*b;
+	unsigned long long	i;
 
 	b = (t_struct *)a;
 	i = 0;
 	while (i < 1000000)
-	{
-		pthread_mutex_lock(&b->mutex);
-		b->x++;
-		pthread_mutex_unlock(&b->mutex);
 		i++;
-	}
+	pthread_mutex_lock(&b->mutex);
+	b->x += i;
+	pthread_mutex_unlock(&b->mutex);
 	return (NULL);
 }
 
@@ -54,13 +52,13 @@ int	main(int argc, char **argv)
 	pthread_mutex_init(&a.mutex, NULL);
 	while (i < n)
 		if ((pthread_create(&t[i++], NULL, &rotine, (void *)&a)))
-			return (1);
+			return (printf("create\n"));
 	i = 0;
 	while (i < n)
 		if (pthread_join(t[i++], NULL))
-			return (1);
+			return (printf("join\n"));
 	pthread_mutex_destroy(&a.mutex);
-	printf("x: %d\n", a.x);
+	printf("x: %llu\n", a.x);
 	free(t);
 	return (0);
 }
